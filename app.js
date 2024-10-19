995f590305dfdf219f4aa9b23ea01706ca79a800
firdhousfiru <firdhouskh2002@gmail.com>
2024-10-19 17:50:15 +0530

    signup completed

--- app.js
+++ app.js
@@ -3,13 +3,42 @@ const mongoose = require("mongoose")
 const cors = require("cors")
 const bcrypt =require("bcrypt")
 const jwt = require("jsonwebtoken")
+const userModel=require("./models/users")
 
 let app=express()
+app.use(express.json())
+app.use(cors())
 
-app.get("/",(req,res)=>{
-    res.send("hello")
+mongoose.connect("mongodb+srv://firdhouskh:kunjumol@cluster0.h3qcl.mongodb.net/musjidapp?retryWrites=true&w=majority&appName=Cluster0")
 
+app.post("/signup",async(req,res)=>{
+    
+    let input=req.body
+    let hashedPassword=bcrypt.hashSync(req.body.password,10)
+    console.log(hashedPassword)
+    req.body.password=hashedPassword
+    
+    let check = userModel.find({email:req.body.email}).then(
+        (items)=>{
+
+
+    
+
+        if(items.length>0){
+            res.json({"status":"email id already exists"})
+
+        }else{
+            let result=new userModel(input)
+             result.save()
+            res.json({"status":"success"})
+        }
+    }
+).catch(
+(error)=>{}
+)
 })
+
+
 
 app.listen(3030,()=>{
     console.log("server started")
